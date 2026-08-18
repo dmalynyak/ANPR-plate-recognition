9b647b0d7f0d22226ce4d53b2d3cfca8977908c9
dmalynyak <dmytro1malynyak@gmail.com>
2026-08-19 01:04:06 +0200

    add test mAP

--- src/training/yolov2_train.py
+++ src/training/yolov2_train.py
@@ -47,7 +47,6 @@ def train_logic(model, optimizer, scheduler_state, anchors, detection_criterion,
     scheduler = yolov2_train_architecture.build_scheduler(optimizer, epochs=50, steps_per_epoch=steps_per_epoch)
     if scheduler_state is not None:
         scheduler.load_state_dict(scheduler_state)
-    best_val_loss = float("inf")
     best_map = 0.0
     for epoch in range(epoch_start, epoch_end):
 
@@ -57,7 +56,6 @@ def train_logic(model, optimizer, scheduler_state, anchors, detection_criterion,
         start = time.perf_counter()
         print("calculating validation loss...")
         val_loss = yolov2_eval_architecture.val_loss(model, val_loader, detection_criterion, device)
-        # yolov2_dataload.save_model_state(model, optimizer, epoch, scheduler, save_path=save_path, name='latest')
         end = time.perf_counter()
         elapsed = end - start
         with open(save_path + "/val_metrics.csv", "a") as f:
@@ -65,13 +63,11 @@ def train_logic(model, optimizer, scheduler_state, anchors, detection_criterion,
                 epoch,   train_loss,           val_loss, ))
         print(f"epoch: {epoch} train loss: {train_loss:.3f}, val loss: {val_loss}, time val_loss: {elapsed:.3f}s.")
 
-        # if val_loss < best_val_loss:
-        #     yolov2_dataload.save_model_state(model, optimizer, epoch, scheduler, anchors, save_path=save_path, name=f"best_loss_epoch{epoch}")
 
         if epoch % 5 == 0:
             print(f"epoch {epoch}, calculating validation mAP...")
             start = time.perf_counter()
-            metrics = yolov2_eval_architecture.val_map(model,anchors, img_val_path, labels_val_path, device)
+            metrics = yolov2_eval_architecture.val_map(model, anchors, img_val_path, labels_val_path, device)
 
             with open(save_path + "/val_metrics.csv", "a") as f:
                 f.write("epoch_{}, val_loss: {:.4f}, mAP@0.5: {:.4f},  mAP@0.75: {:.4f},  mAP@[.5:.95]: {:.4f}\n".format(
@@ -82,11 +78,17 @@ def train_logic(model, optimizer, scheduler_state, anchors, detection_criterion,
             print(f"took {elapsed:.3f} seconds")
             if metrics["mAP_5095"] > best_map:
                 best_map = metrics["mAP_5095"]
-                yolov2_dataload.save_model_state(model, optimizer, epoch, scheduler, anchors, save_path=save_path, name=f"best_mAP_epoch{epoch}.pt")
+                yolov2_dataload.save_model_state(model, optimizer, epoch, scheduler, anchors, save_path=save_path, name=f"best_mAP.pt")
                 yolov2_dataload.save_model_weights(model, anchors, save_path=save_path, name='best_weights.pt')
 
         yolov2_dataload.save_model_state(model, optimizer, epoch, scheduler, anchors, save_path=save_path, name=f"last.pt")
 
+    checkpoint = torch.load(f"{save_path}/best_mAP.pt", map_location=device)
+    model.load_state_dict(checkpoint["state_dict"])
+    test_metrics = yolov2_eval_architecture.val_map(model, anchors, img_test_path, labels_test_path, device)
+    with open(save_path + "/val_metrics.csv", "a") as f:
+        f.write("test_mAP@0.5: {:.4f},  test_mAP@0.75: {:.4f},  test_mAP@[.5:.95]: {:.4f}\n".format(
+            test_metrics["mAP_50"], test_metrics["mAP_75"], test_metrics["mAP_5095"]))
 
 
 def train_from_zero(save_path, device):
@@ -107,7 +109,7 @@ def train_from_zero(save_path, device):
     detection_criterion = yolov2_train_architecture.YOLOv2Loss(anchors).to(device)
 
     scheduler_state = None
-    train_logic(model, optimizer, scheduler_state, anchors, detection_criterion, epoch_start=0, epoch_end=50, save_path=save_path, device=device)
+    train_logic(model, optimizer, scheduler_state, anchors, detection_criterion, epoch_start=0, epoch_end=50 + 1, save_path=save_path, device=device)
 
 
 def train_resume(load_path, save_path, device):
@@ -127,7 +129,7 @@ def train_resume(load_path, save_path, device):
     print(f"Resuming training from epoch {epoch_start} with anchors:\n{anchors}")
 
     train_logic(model, optimizer, checkpoint["scheduler"], anchors, detection_criterion,
-                epoch_start + 1, epoch_end=50, save_path=save_path, device=device)
+                epoch_start + 1, epoch_end=50 + 1, save_path=save_path, device=device)
 
 
 def main():
